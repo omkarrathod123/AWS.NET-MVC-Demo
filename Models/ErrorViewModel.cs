16c7d948d99cc04c6e31f64a6da374b3837f8ccc
omkarrathod123 <omkarrathod0705@gmail.com>
2026-02-23 11:48:34 +0530

    Init project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AWS.NET_MVC_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
