afe4e9effadbb7a4861e539dcd94626bd63d61ec
omkarrathod123 <omkarrathod0705@gmail.com>
2026-02-26 00:01:04 +0530

    Adding Data Annotations

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,14 +6,20 @@ namespace AWS.NET_MVC_Demo.Models
     public class Product
     {
         [Key]
+        [Display(Name = "Product ID")]
         public int productID { get; set; }
+        [Display(Name = "Product Name")]
         public string productName { get; set; }
+        [Display(Name = "Description")]
         public string productDescription { get; set; }
+        [Display(Name = "Product Type")]
         public string productType { get; set; }
         [Range(0, 1000)]
         [NotNull]
+        [Display(Name = "Product Quantity")]
         public int quantity { get; set; }
         [Required]
+        [Display(Name = "Product Price")]
         public int price { get; set; }
     }
 }
